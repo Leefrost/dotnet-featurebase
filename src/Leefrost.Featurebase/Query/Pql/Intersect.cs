93ad4b58ff2ca5ae8eade301f9bf2f2938eaba27
Leefrost <evertodante@gmail.com>
2023-12-17 21:45:43 +0200

    Added Intersect

---
+++ src/Leefrost.Featurebase/Query/Pql/Intersect.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace Leefrost.Featurebase.Query.Pql;
+public class Intersect : PqlRowQuery
+{
+    private readonly List<PqlRowQuery> _rows = [];
+
+    public Intersect(PqlRowQuery row1, PqlRowQuery row2)
+    {
+        _rows.Add(row1);
+        _rows.Add(row2);
+    }
+
+    public Intersect(IEnumerable<PqlRowQuery> rows)
+    {
+        _rows = rows.ToList();
+    }
+    
+    public override string Build()
+    {
+        var builder = new StringBuilder();
+        builder.Append("Intersect(");
+        builder.Append(string.Join(',', _rows.Select(row => row.Build())));
+        builder.Append(')');
+
+        return builder.ToString();
+    }
+}
--- tests/Leefrost.Featurebase.Tests/Query/QueryTests.cs
+++ tests/Leefrost.Featurebase.Tests/Query/QueryTests.cs
@@ -5,12 +5,22 @@ namespace Leefrost.Featurebase.Tests.Query;
 public class QueryTests
 {
     [Fact]
-    public void OnlyMalesWithDiscount_QueryIsValid()
+    public void Unique_OnlyMalesWithDiscount_QueryIsValid()
     {
         var row = new Distinct(new Row("gender", "male"), "has_discount");
 
         var result = row.Build();
 
         result.Should().Be("Distinct(Row(gender=male), field=has_discount)");
+    }
+
+    [Fact]
+    public void Unique_FromLvivOrOlderThan21_QueryIsValid()
+    {
+        var row = new Distinct(new Intersect(new Row("age>21"), new Row("city", "Lviv")), "has_discount");
+
+        var result = row.Build();
+
+        result.Should().Be("Distinct(Intersect(Row(age>21),Row(city=Lviv)), field=has_discount)");
     }
 }

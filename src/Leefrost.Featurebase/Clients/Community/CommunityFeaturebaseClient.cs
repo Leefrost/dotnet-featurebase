2b445566275b38c3d71b3b55a255d8bf4bb48088
Leefrost <evertodante@gmail.com>
2024-01-15 21:17:35 +0200

    Update signature

--- src/Leefrost.Featurebase/Clients/Community/CommunityFeaturebaseClient.cs
+++ src/Leefrost.Featurebase/Clients/Community/CommunityFeaturebaseClient.cs
@@ -1,4 +1,5 @@
 ﻿using Leefrost.Featurebase.Clients.Community.Responses;
+using Leefrost.Featurebase.Pql;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -34,6 +35,27 @@ public sealed class CommunityFeaturebaseClient : IFeaturebaseClient
         _httpClient = httpClient;
         _options = options.Value;
         _logger = logger;
+    }
+
+    public async Task<TResult> ExecuteAsync<TResult>(Query query, CancellationToken cancellationToken)
+    {
+        var content = new StringContent(query.Build());
+
+        using var response = await _httpClient.PostAsync(PqlEndpoint, content, cancellationToken);
+        await response.ThrowIfNotSuccessfulAsync(cancellationToken);
+
+        var result = await response.FetchAsync<TResult>(cancellationToken);
+        return result;
+    }
+
+    public Task<IReadOnlyList<TResult>> ExecuteManyAsync<TResult>(Query query, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> CountAsync(CountQuery query, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
     }
 
     public async Task<long> CountAsync(string query, CancellationToken cancellationToken)
--- src/Leefrost.Featurebase/Clients/IFeaturebaseClient.cs
+++ src/Leefrost.Featurebase/Clients/IFeaturebaseClient.cs
@@ -1,7 +1,14 @@
-﻿namespace Leefrost.Featurebase.Clients;
+﻿using Leefrost.Featurebase.Pql;
+
+namespace Leefrost.Featurebase.Clients;
 
 public interface IFeaturebaseClient: IDisposable
 {
+    Task<TResult> ExecuteAsync<TResult>(Query query, CancellationToken  cancellationToken);
+    Task<IReadOnlyList<TResult>> ExecuteManyAsync<TResult>(Query query, CancellationToken cancellationToken);
+    Task<int> CountAsync(CountQuery query, CancellationToken cancellationToken);
+
+
     Task<long> CountAsync(string query, CancellationToken cancellationToken);
     Task<IReadOnlyList<string>> SelectAsync(string query, CancellationToken cancellationToken);
     Task<string> ExecuteRawPqlAsync(string query, CancellationToken cancellationToken);
